8e5e521e084527547d4c4c1bd686b9837ea7b401
Nicholas Veras <nicholasveras@MacBook-Air-de-Nicholas.local>
2023-12-28 09:37:50 -0300

    feat/createIndex

---
+++ src/routes/index.js
@@ -0,0 +1,22 @@
+import {createNativeStackNavigator} from '@react-navigation/native-stack'
+
+import Home from '../pages/Home'
+import Cart from '../pages/Cart'
+
+const Stack = createNativeStackNavigator();
+
+export default function Routes(){
+    return(
+        <Stack.Navigator>
+            <Stack.Screen
+            name='Home'
+            component={Home}
+            />
+
+        <Stack.Screen
+            name='Cart'
+            component={Cart}
+            />
+        </Stack.Navigator>
+    )
+}
